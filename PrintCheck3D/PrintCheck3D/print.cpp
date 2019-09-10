908179fc2d6028e784a47149ab96f549606f6e0e
Stephen Donchez <sdonchez@villanova.edu>
2019-09-09 21:18:32 -0400

    Initial work on print class. Function and struct declarations, no definitions.

---
+++ PrintCheck3D/PrintCheck3D/print.cpp
@@ -0,0 +1,34 @@
+/*
+*	print.c:
+*		Facilitates the monitoring of a printjob, and serves as the calling
+*		class for the sending of commands to the printer via RAMBoInterface.h
+********************************************************************************
+*	This file is part of PrintCheck3D
+*	Copyright(C) 2019 Stephen Donchez, Jared Rymsza, Robert Pink
+*
+*	This program is free software : you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.If not, see < https://www.gnu.org/licenses/>.
+*******************************************************************************/
+
+#include "print.h"
+
+
+
+print::print()
+{
+}
+
+
+print::~print()
+{
+}
