5b756ed433d0b36c4e747582e4aace5ef571498e
jrymsza <48486636+jrymsza@users.noreply.github.com>
2019-12-05 10:51:38 -0500

    quick fix

--- PrintCheck3D/PrinterControlDemo/main.cpp
+++ PrintCheck3D/PrinterControlDemo/main.cpp
@@ -8,33 +8,34 @@ using namespace std;
 int main()
 {
 	RAMBoInterface printer = RAMBoInterface();
-	gCodeTransfer printJob = gCodeTransfer("/media/pi/PRINT3D/rocktopus-sample.gcode");
+	gCodeTransfer printJob = gCodeTransfer("/media/pi/PRINT3D/sample.gcode");
 	int choice;
-	string menu = "Enter an option from the choices below\n1) insert a filament change\n2) insert a recover\n3) insert a stop\n4) show arc movement\n5) print rocktopus\n6) quit";
+	string menu = "Enter an option from the choices below\n1) insert a filament change\n2) insert a recover\n3) insert a stop\n4) show arc movement\n5) print gcode\n6) quit";
 	while (true) {
 		cout << menu << endl;
 		int read = scanf("%d", &choice);
 		switch (choice)
 		{
 		case 1:
-			printer.pausePrint();
 			cout << "filament swap initiated\n" << endl;
+			printer.pausePrint();
 			break;
 		case 2:
-			printer.resumePrint();
 			cout << "print resume initiated\n" << endl;
+			printer.resumePrint();
 			break;
 		case 3:
-			printer.stopPrint();
 			cout << "print stop initiated\n" << endl;
+			printer.stopPrint();
 			break;
 		case 4:
-			printer.printArc();
 			cout << "circle print initiated\n" << endl;
+			printer.printArc();
 			break;
 		case 5:
+			cout << "printing gcode\n" << endl;
 			printer.runPrintJob(printJob.getVector());
-			cout << "printing rocktopus\n" << endl;
+			
 			break;
 		case 6:
 			return 0;
